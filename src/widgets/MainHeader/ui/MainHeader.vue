509d7cf0e9b5b55bcbedf9a6a33f7f96bb7c57b0
Сайсанов Дмитрий Алексеевич <saysanov2018@mail.ru>
2025-10-31 18:57:34 +0500

    вынес роутер пуш в отдельную функцию, фикс футера

--- src/widgets/MainHeader/ui/MainHeader.vue
+++ src/widgets/MainHeader/ui/MainHeader.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import RecipeMainIcon from '@/shared/icons/RecipeMainIcon.vue'
+import { RECIPES_ROUTE_NAMES } from '@/shared/router/routes'
+
+const router = useRouter()
+
+function handlePushMainPage() {
+  router.push({ name: RECIPES_ROUTE_NAMES.ALL_RECIPES })
+}
 </script>
 
 <template>
   <div class="flex h-[68px] w-full flex-row items-center justify-start gap-10 border-b border-gray-300 bg-white px-10">
     <div
       class="flex cursor-pointer flex-row items-center gap-4"
-      @click="$router.push(`/`)"
+      @click="handlePushMainPage"
     >
       <ElIcon size="40">
         <RecipeMainIcon />
